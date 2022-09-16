a6ef6885162a83a39aa7e50b186108f66de81e16
Sreevatsank Kadaveru <skadaveru@ucsd.edu>
2022-09-16 05:07:32 -0700

    Minor bugfixes, Added docker-compose and Dockerfile and job.yml

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM wcsng-36.ucsd.edu:5000/matlab:u20.04_m2022a_july29
+FROM wcsng-36.ucsd.edu/matlab:u20.04_m2022a_july29
 
 ARG USER=scisrs
 ARG USERDIR=/home/${USER}
@@ -9,7 +9,7 @@ RUN mkdir ${USERDIR}/fft_boxing
 
 COPY --chown=${USER} searchlight ${USERDIR}/fft_boxing/searchlight
 
-COPY --chown=${USER} fft_boxing.m ${USERDIR}/fft_boxing
+COPY --chown=${USER} . ${USERDIR}/fft_boxing
 
 USER ${USER}
 WORKDIR ${USERDIR}/fft_boxing
--- fft_boxing.m
+++ fft_boxing.m
@@ -5,6 +5,9 @@ startup;
 basepath = getenv('BASEPATH');
 time_slice=getenv('TIME_SLICE'); % sec
 
+% basepath = '/tmp/soi-sc16-lo-scaled';
+% time_slice=0.1; % sec
+
 files=dir(basepath);
 is_dir=[files.isdir];
 dirs=files(is_dir);
@@ -21,17 +24,17 @@ for i=1:length(survey)
     disp("Running on dir: " + archive)
     
     % Valid radios are 1-5
-    num_radios = 5
+    num_radios = 5;
     % Only channel 0 is currently collected
-    channel = 0
+    channel = 0;
     
-    boxdir= archive_path+'/'+"box2d"
-    if ~exists(boxdir, 'dir'):
-        mkdir(boxdir)
+    boxdir= archive_path+'/'+"box2d";
+    if ~exist(boxdir, 'dir')
+        mkdir(boxdir);
     end
     
     datafile = dir(archive_path+'/'+'fft-archive-0-0-0000.sigmf-data');
-    metafile_json = jsondecode(char(fread(fopen(archive_path+'/'+'fft-archive-0-0-0000.sigmf-meta'),inf)'));
+    metadata= jsondecode(char(fread(fopen(archive_path+'/'+'fft-archive-0-0-0000.sigmf-meta'),inf)'));
 
     nfft = metadata.global.scisrs_fft_size;
     fs = metadata.global.core_sample_rate;
@@ -39,31 +42,31 @@ for i=1:length(survey)
     
     total_samples = datafile.bytes/4;
     
-    for radio_i=1:num_radios:
+    for radio_i=1:num_radios
         filename=sprintf("fft-archive-%d-%d-0000", radio_i,channel);
         fp = archive_path+'/'+filename+'.sigmf-data';
         fid = fopen(fp);
         
         count = ceil(time_slice*fs/nfft)*nfft;
         
-        nChunk=ceil(total_samples/count)
+        nChunk=ceil(total_samples/count);
         boxingResults = cell(nChunk, 1);
         for iChunk=1:nChunk
             % fseek(fid,file_offset,'bof');
             
-            data=fread(fid,count,'float')
+            data=fread(fid,count,'float');
             data = reshape(data, nfft, []).';
             data = fftshift(data, 2);
             
-            elapsed = (iChunk-1)*count / fs
-            disp("Elapsed " + str(elapsed) +"s")
+            elapsed = (iChunk-1)*count / fs;
+            disp("Elapsed " + string(elapsed) +"s");
             
             dataChan = ChannelizerData(data, fs/nfft, getIJVector(nfft, 1, fs/nfft), elapsed);
             dataChan.isAlreadyPower = true;
             c = ConvContextualize(dataChan);
             c.process;
             c.plotFinalBoxes;
-            boxingResults{iChunk} = BoxingResults(dataChunk, ...
+            boxingResults{iChunk} = BoxingResults(dataChan, ...
                 c.centerArray_Hz - c.widthArray_Hz/2, ...
                 c.centerArray_Hz + c.widthArray_Hz/2, ...
                 c.centerArray_s - c.heightArray_s/2, ...
