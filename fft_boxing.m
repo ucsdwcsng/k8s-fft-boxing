6096a3a17e63a00b0ca67f2dcc48bb813e82c12c
Sreevatsank Kadaveru <skadaveru@ucsd.edu>
2022-09-18 04:40:12 -0700

    Major: Jobs now dont iterate over child directories, each job can only process one capture Added batch job launch script

--- fft_boxing.m
+++ fft_boxing.m
@@ -8,77 +8,66 @@ time_slice=getenv('TIME_SLICE'); % sec
 % basepath = '/tmp/soi-sc16-lo-scaled';
 % time_slice=0.1; % sec
 
-files=dir(basepath);
-is_dir=[files.isdir];
-dirs=files(is_dir);
+tmp=split(basepath, '/');
+archive=tmp(end);
+archive_path=basepath;
+disp("Running on dir: " + archive)
 
-survey={dirs(3:end).name};
+% Valid radios are 1-5
+num_radios = 5;
+% Only channel 0 is currently collected
+channel = 0;
 
+boxdir= archive_path+'/'+"box2d";
+if ~exist(boxdir, 'dir')
+    mkdir(boxdir);
+end
 
-for i=1:length(survey)
-    
-    archive=survey{i};
+datafile = dir(archive_path+'/'+'fft-archive-0-0-0000.sigmf-data');
+metadata= jsondecode(char(fread(fopen(archive_path+'/'+'fft-archive-0-0-0000.sigmf-meta'),inf)'));
 
-    archive_path=basepath+'/'+archive;
+nfft = metadata.global.scisrs_fft_size;
+fs = metadata.global.core_sample_rate;
+ifnom = metadata.captures.core_frequency;
 
-    disp("Running on dir: " + archive)
-    
-    % Valid radios are 1-5
-    num_radios = 5;
-    % Only channel 0 is currently collected
-    channel = 0;
-    
-    boxdir= archive_path+'/'+"box2d";
-    if ~exist(boxdir, 'dir')
-        mkdir(boxdir);
-    end
-    
-    datafile = dir(archive_path+'/'+'fft-archive-0-0-0000.sigmf-data');
-    metadata= jsondecode(char(fread(fopen(archive_path+'/'+'fft-archive-0-0-0000.sigmf-meta'),inf)'));
+total_samples = datafile.bytes/4;
 
-    nfft = metadata.global.scisrs_fft_size;
-    fs = metadata.global.core_sample_rate;
-    ifnom = metadata.captures.core_frequency;
+for radio_i=0:num_radios-1
+    filename=sprintf("fft-archive-%d-%d-0000", radio_i,channel);
+    fp = archive_path+'/'+filename+'.sigmf-data';
+    fid = fopen(fp);
     
-    total_samples = datafile.bytes/4;
+    disp("Running file: " + string(filename))
+
+    count = ceil(time_slice*fs/nfft)*nfft;
     
-    for radio_i=0:num_radios-1
-        filename=sprintf("fft-archive-%d-%d-0000", radio_i,channel);
-        fp = archive_path+'/'+filename+'.sigmf-data';
-        fid = fopen(fp);
+    nChunk=ceil(total_samples/count);
+    boxingResults = cell(nChunk, 1);
+    for iChunk=1:nChunk
+        % fseek(fid,file_offset,'bof');
         
-        disp("Running file: " + string(filename))
-
-        count = ceil(time_slice*fs/nfft)*nfft;
+        data=fread(fid,count,'float');
+        data = reshape(data, nfft, []).';
+        data = fftshift(data, 2);
         
-        nChunk=ceil(total_samples/count);
-        boxingResults = cell(nChunk, 1);
-        for iChunk=1:nChunk
-            % fseek(fid,file_offset,'bof');
-            
-            data=fread(fid,count,'float');
-            data = reshape(data, nfft, []).';
-            data = fftshift(data, 2);
-            
-            elapsed = (iChunk-1)*count / fs;
-            disp("Elapsed " + string(elapsed) +"s");
-            
-            dataChan = ChannelizerData(data, fs/nfft, getIJVector(nfft, 1, fs/nfft), elapsed);
-            dataChan.isAlreadyPower = true;
-            c = ConvContextualize(dataChan);
-            c.process;
-            % c.plotFinalBoxes;
-            boxingResults{iChunk} = BoxingResults(dataChan, ...
-                c.centerArray_Hz - c.widthArray_Hz/2, ...
-                c.centerArray_Hz + c.widthArray_Hz/2, ...
-                c.centerArray_s - c.heightArray_s/2, ...
-                c.centerArray_s + c.heightArray_s/2, ...
-                c.aboveNoiseFloorRaw_dB, ...
-                c.aboveNoiseFloorProcessed_dB);         
+        elapsed = (iChunk-1)*count / fs;
+        disp("Elapsed " + string(elapsed) +"s");
+        
+        dataChan = ChannelizerData(data, fs/nfft, getIJVector(nfft, 1, fs/nfft), elapsed);
+        dataChan.isAlreadyPower = true;
+        c = ConvContextualize(dataChan);
+        c.process;
+        % c.plotFinalBoxes;
+        boxingResults{iChunk} = BoxingResults(dataChan, ...
+            c.centerArray_Hz - c.widthArray_Hz/2, ...
+            c.centerArray_Hz + c.widthArray_Hz/2, ...
+            c.centerArray_s - c.heightArray_s/2, ...
+            c.centerArray_s + c.heightArray_s/2, ...
+            c.aboveNoiseFloorRaw_dB, ...
+            c.aboveNoiseFloorProcessed_dB);         
 
-            boxingResults{iChunk}.channelizerData=[];
-            java.lang.Runtime.getRuntime().gc;
-        end
-        save(boxdir+'/'+filename+'.mat','boxingResults','-v7.3');
+        boxingResults{iChunk}.channelizerData=[];
+        java.lang.Runtime.getRuntime().gc;
     end
+    save(boxdir+'/'+filename+'.mat','boxingResults','-v7.3');
 end
---
+++ launch_jobs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo 'Launching Jobs'
+echo '---------------------------------------------------------------------------------------------'
+
+print_help(){
+    echo -e "Usage: ./launch_jobs.sh [-p <jobname_prefix>] <jobs_file>"
+    echo -e "\t\t jobname_prefix: \tValue prepended to jon_name (Default: '')"
+    echo
+}
+
+jobname_prefix=""
+while getopts p:h flag
+do
+    case "${flag}" in
+        p) jobname_prefix="${OPTARG}."
+            shift;;
+        *)
+            print_help;
+            exit -1;
+        ;;
+    esac
+    shift;
+done
+
+if [[ $# -ne 1 ]]
+then
+    print_help;
+    exit -1;
+fi
+
+jobs_file="$1"
+
+echo "jobs_file: $jobs_file"
+echo 
+
+jobid=0;
+for d in $(cat $jobs_file)
+do
+    job_major=$(echo $d | sed -e 's/.*\/(.*?)\/(.*)$/\1/')
+    job_minor=$(echo $d | sed -e 's/.*\/(.+?)\/(.+)$/\2/')
+    job_name="$job_major.$jobid"
+
+    echo "Job: $job_major/$job_minor"
+    echo "job_name: $job_name"
+
+    # helm install $job_name chart/fft_boxing/ \
+    #     --set jobParam.name="$job_name" \
+    #     --set jobParam.dataDir="$d"    \
+    #     --set jobParam.description="dir: $d"    
+
+
+    ((jobid=jobid+1))
+done
